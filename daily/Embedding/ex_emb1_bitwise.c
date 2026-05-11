bc967b06a952f738dac9ab21eb52a2f3e016057a
TianBaoChiu <seedage57309@gmail.com>
2026-05-12 01:13:46 +0800

    [update] complete ex_emb1_bitwise with bit manipulation macros

--- daily/Embedding/ex_emb1_bitwise.c
+++ daily/Embedding/ex_emb1_bitwise.c
@@ -8,28 +8,31 @@
 #include <stdint.h>
 
 // TODO 1: 實作以下巨集 (Macro) 來操作 bit (提示：使用 <<, |, &, ~, ^)
-#define SET_BIT(REG, BIT)     /* 將 REG 的第 BIT 位元設為 1 */
-#define CLEAR_BIT(REG, BIT)   /* 將 REG 的第 BIT 位元設為 0 */
-#define TOGGLE_BIT(REG, BIT)  /* 將 REG 的第 BIT 位元反轉 */
-#define CHECK_BIT(REG, BIT)   /* 檢查 REG 的第 BIT 位元是否為 1 (回傳 0 或非 0) */
+#define SET_BIT(REG, BIT)     (REG |= 1 << BIT)/* 將 REG 的第 BIT 位元設為 1 */
+#define CLEAR_BIT(REG, BIT)   (REG &= ~(1 << BIT))/* 將 REG 的第 BIT 位元設為 0 */
+#define TOGGLE_BIT(REG, BIT)  (REG ^= 1 << BIT)/* 將 REG 的第 BIT 位元反轉 */
+#define CHECK_BIT(REG, BIT)   ((REG >> BIT) & 1)/* 檢查 REG 的第 BIT 位元是否為 1 (回傳 0 或非 0) */
 
 int main() {
     uint32_t PORTA = 0x00000000; // 模擬一個 32-bit 硬體暫存器
 
     // TODO 2: 使用 SET_BIT 將第 3 個位元 (Bit 3) 設為 1
     // 預期 PORTA 變成 0x08
+    SET_BIT(PORTA, 3);
     
     // TODO 3: 使用 SET_BIT 將第 7 個位元 (Bit 7) 設為 1
     // 預期 PORTA 變成 0x88
-    
+    SET_BIT(PORTA, 7);
     // TODO 4: 使用 CLEAR_BIT 將第 3 個位元清零
     // 預期 PORTA 變成 0x80
-
+    CLEAR_BIT(PORTA, 3);
     // TODO 5: 使用 TOGGLE_BIT 反轉第 0 個位元
     // 預期 PORTA 變成 0x81
-
+    TOGGLE_BIT(PORTA, 0);
     // TODO 6: 使用 CHECK_BIT 檢查第 7 個位元和第 3 個位元，並寫 if 判斷印出結果
     // 例如：if (CHECK_BIT(PORTA, 7)) { printf("Bit 7 is SET\n"); }
+    if(CHECK_BIT(PORTA, 7)) {printf("Bit 7 is SET\n");}
+    if(CHECK_BIT(PORTA, 3)) {printf("Bit 3 is SET\n");}
     
     
     printf("Final PORTA value: 0x%02X\n", PORTA); // 預期輸出 0x81
